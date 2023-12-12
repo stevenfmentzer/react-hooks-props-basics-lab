d7e4fd03e9b20c621d3004642eef74e9d70cf664
Steven Mentzer <stevenfmentzer@gmail.com>
2023-12-12 11:23:40 -0500

    finished deliverables

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,22 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {checkBio(props.bio)}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
+}
+
+function checkBio(bio){
+  if(bio && bio != ""){
+    return <p>{bio}</p>
+  } 
 }
 
 export default About;
